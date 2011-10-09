b495805ba6c36926295f22bf9ad1769262bea4ba
wickhb <wickhb@rpi.edu>
2011-10-09 13:25:18 -0400

    Adding initial project

---
+++ MovieSearch.py
@@ -0,0 +1,73 @@
+
+__author__="Daniel Zhao"
+__date__ ="$Sep 30, 2011 7:15:21 PM$"
+
+
+#import abc
+from SearchBase import SearchBase
+
+tagline = '   ***DONATE TO SUPPORT INDEPENDENT FILM!***'
+
+class MovieSearch(SearchBase):
+    results = None
+    keywords = {}
+
+    def search(self, query, rpp):
+        self.results = super(MovieSearch, self).search(query, rpp)
+        self.reorder()
+        #self.printResults()
+
+    def printResults(self):
+        for res in self.results:
+            print res.title.encode("utf8")
+            print res.desc.encode("utf8")
+            print res.url.encode("utf8")
+            print
+
+    def initDictionary(self):
+        key_file = open("keywords.txt",'r')
+        count = 1
+        for line in key_file:
+            for word in line.split():
+                self.keywords[word]=count
+                count = count+1
+
+    def reorder(self):
+        self.initDictionary()
+        value = 0
+        orders = []
+        pair = ()
+
+        #search titles and descriptions for keywords
+        for res in self.results:
+            tmpTitle = (res.title.encode('utf-8').lower().strip('().,:-\'\"')).split(" ")
+            tmpDesc = (res.desc.encode('utf-8').lower().strip('().,:-\'\"')).split(" ")
+            for key in self.keywords.keys():
+                for t in tmpTitle:
+                    if key == t:
+                        value+=self.keywords[key]
+                for t in tmpDesc:
+                    if key == t:
+                        value+=self.keywords[key]
+            pair = res, value
+            orders.append(pair)
+            pair = ()
+            value = 0
+
+        #order results based on values
+        def cmpfun(a,b):
+            return cmp(b[1],a[1])
+        orders.sort(cmpfun)
+
+
+        for i in orders:
+            if i[1] <> 0:
+                print tagline
+                
+            print i[0].title.encode("utf8")#, "RANK = ", i[1]
+            print i[0].desc.encode("utf8")
+            print i[0].url.encode("utf8")
+            print
+
+
+
---
+++ webscience.py
@@ -0,0 +1,9 @@
+
+__author__="Daniel Zhao"
+__date__ ="$Sep 30, 2011 2:52:50 PM$"
+
+
+from MovieSearch import MovieSearch
+
+test = MovieSearch()
+test.search("doberman", 200)
